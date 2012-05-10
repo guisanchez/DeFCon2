e0ab71692fa10d217438b51578d94a7d28a1a464
B0RJA <borja.latorre@eead.csic.es>
2012-05-10 15:06:52 +0300

    beautify input.js

--- input.js
+++ input.js
@@ -1,23 +1,24 @@
 {
-    "time" : 
+  "time" :
     {
-	"cfl": 0.9,
-	"end": 300.0,
-	"vtklag": 5.0,
-	"stdprint": 50
+      "cfl"      : 0.9,
+      "end"      : 300.0,
+      "vtklag"   : 5.0,
+      "stdprint" : 50
     },
-    "params" : 
+  
+  "params" :
     {
-	"tanphi": 0.226295,
-	"Xi": 0.004888,
-	"k": 1.0
+      "tanphi" : 0.226295,
+      "Xi"     : 0.004888,
+      "k"      : 1.0
     },
-    "preproc_mesh": 1,
-    "preproc_err": 1,
-    "standard_out": 1,
-    "compare_result": 0,
-    "generate_vtk": 1,
-    "mesh": "malla0.txt",
-    "h_initial": "init0.txt"
+    
+  "mesh"           : "mesh.txt",
+  "h_initial"      : "h_initial.txt",
+  "preproc_mesh"   : 1,
+  "preproc_err"    : 1,
+  "standard_out"   : 1,
+  "generate_vtk"   : 1,
+  "compare_result" : 0
 }
-	
