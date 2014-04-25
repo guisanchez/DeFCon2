feb4c3ba0a09346b494acb6a7a4bf63238d8b250
Guillermo Sanchez <guillmor@gmail.com>
2014-04-25 22:10:56 +0200

    new input.js file

--- input.js
+++ input.js
@@ -1,24 +1,33 @@
 {
-  "time" :
+    "time" : 
     {
-      "cfl"      : 0.9,
-      "end"      : 300.0,
-      "vtklag"   : 5.0,
-      "stdprint" : 50
+	"cfl": 0.9,
+	"end": 100.0,
+	"vtklag": 2.0,
+	"stdprint": 50
     },
-  
-  "params" :
+    "friclaw": "voellmy",
+    "params" : 
     {
-      "tanphi" : 0.226295,
-      "Xi"     : 0.004888,
-      "k"      : 1.0
+	"rho" : 1.0,
+	"yieldstress" : 0.1,
+	"mu" : 35.0,
+	"tanphi": 0.15,
+	"Xi": 0.01,
+	"k": 1.0
     },
-    
-  "mesh"           : "mesh.txt",
-  "h_initial"      : "h_initial.txt",
-  "preproc_mesh"   : 1,
-  "preproc_err"    : 1,
-  "standard_out"   : 1,
-  "generate_vtk"   : 1,
-  "compare_result" : 0
+    "preproc_mesh": 1,
+    "preproc_err": 1,
+    "standard_out": 1,
+    "compare_result": 1,
+    "generate_vtk": 1,
+    "mesh": "meshfile.asc",
+    "h_initial": "hfile.asc",
+    "h_observ": "deposit.asc",
+    "stop" :
+    {
+	"stime" : 100.0,
+	"Ekstop" : 10000.0
+    }
 }
+	
